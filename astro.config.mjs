de7e19010dfecfb996cf1204726181d233c38938
javier25arroyo <javier25arojas@gmail.com>
2025-07-29 23:18:07 -0600

    Configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,5 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   site: 'https://javier25arroyo.github.io',
   base: '/Portafolio',
-  integrations: [
-    tailwind({
-      applyBaseStyles: false, // Ya que usamos nuestros propios estilos base
-    })
-  ],
+  integrations: [tailwind()],
 });
